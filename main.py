b73ce59d051f3e98af04249e8dcb19fb01d26207
Sattyam Mane <127324042+sattyamakasam@users.noreply.github.com>
2023-03-31 10:08:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a + b
         
 def SayHello():
-        print("Hello there From Sattyam")
+        print("Hello there From Sattyam.")
 
 if __name__ == '__main__':
         SayHello()
